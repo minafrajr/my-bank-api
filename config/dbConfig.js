a88caa11cd4c826a2841c928e8fe982aa8838d46
minafrajr <carlos.minafrajr@gmail.com>
2020-08-31 15:21:17 -0300

    add comments

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,9 +1,14 @@
-import mongoose from 'mongoose';
-import accountsModel from '../models/accountsModels.js';
+import mongoose from 'mongoose'
+import accountsModel from '../models/accountsModels.js'
 
-const db = {};
-db.url = process.env.MONGO_URL;
-db.mongoose = mongoose;
-db.accounts = accountsModel(mongoose);
+//objeto de conexão com o banco
+const db = {}
+//a string de conexão com o banco
+db.url = process.env.MONGO_URL
+//o objeto mongoose
+db.mongoose = mongoose
+//o modelo de accounts tranzendo a função accountsModel de ./models/accountsModels.js
+//passando o objeto mongoose como parâmetro
+db.accounts = accountsModel(mongoose)
 
-export { db };
+export { db }
